dc595020072119de59d65011cd29f0af08bd0991
Elliot Erickson <eric0952@gmail.com>
2022-07-08 13:29:09 -0700

    Days of Week
    added weekDaysArr and mapped days of week in the week container

--- components/calandar-view.js
+++ components/calandar-view.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import cal from '../styles/cal.module.scss';
 
+const weekDaysArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const timeSlotArr = [1, 2, 3, 4];
+
 class CalandarView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
     render () {
+        const weekDays = weekDaysArr.map((day) =>
+            <div className={cal.dayContainer}>{day}</div>
+            )
         return (
             <div className={cal.calContainer}>
                 <div className={cal.dateSlide}>
-                    Week of August 7th
+                    <div>arr</div>
+                    <div className={cal.slideText}>
+                        <p>Week of August 7th</p>
+                    </div>
+                    <div>arr</div>
                 </div>
                 <div className={cal.weekContainer}>
-                    week container
+                    <div>arr</div>
+                    {weekDays}
+                    <div>arr</div>
                 </div>
                 <div className={cal.controlContainer}>
                     <div className={cal.lessonControl}>
