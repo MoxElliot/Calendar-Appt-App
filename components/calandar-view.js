dbb6d2d4656b52679da0a409c5bab647c11453a6
Elliot Erickson <eric0952@gmail.com>
2022-07-12 14:56:58 -0500

    time slot buttons
    added new component for the calandar day including buttons for time slots

--- components/calandar-view.js
+++ components/calandar-view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import cal from '../styles/cal.module.scss';
+import CalandarDay from './cal-day';
 import LessonControl from './lesson-control';
 
 const weekDaysArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -8,26 +9,27 @@ const weekDaysArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sa
 const CalandarView = () => {
   
         const weekDays = weekDaysArr.map((day) =>
-            <div className={cal.dayContainer} key={day.toString()}>{day}</div>
+            <div className={cal.dayContainer} key={day.toString()}>{day}<CalandarDay /></div>
             )
 
         return (
             <div className={cal.calContainer}>
                 <div className={cal.dateSlide}>
-                    <div>ar0</div>
+                    <div>aro</div>
                     <div className={cal.slideText}>
                         <p>Week of August 7th</p>
+                       
                     </div>
                     <div>aro</div>
                 </div>
                 <div className={cal.weekContainer}>
-                    <div>aro</div>
                     {weekDays}
-                    <div>ar0</div>
+                    
                 </div>
                 <div className={cal.controlContainer}>
                     <LessonControl className={cal.lessonControl}/>
                 </div>
+                
             </div>
         )
     }
