a91b71623de19154c0b849a1f9322ee1b9d2e478
Elliot Erickson <eric0952@gmail.com>
2022-09-30 13:04:09 -0700

    merged calendar mod
    removed module scss from instructor calendar. will do the same app wide. added in the calendar control for adding new lessons

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
             <title>Home Page</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+      </Head>
     <NavBar />
     <h1>Hello</h1>
     </div>
