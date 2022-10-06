99f35e8a3864dc68dca03fcd0d8a664a2456a87e
Elliot Erickson <eric0952@gmail.com>
2022-10-06 13:21:51 -0700

    added state, onChange, value for adding a lesson to lesson-calendar-control
    Also added a newtime field and moved the repeat day radios. each form has a id, name, value, and onChange property

--- components/instructor-calandar-view/lesson-calandar-control.js
+++ components/instructor-calandar-view/lesson-calandar-control.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { useSelector } from 'react-redux';
 
 
@@ -7,51 +7,122 @@ const repeatOptArr = ['Daily', 'Weekly', 'Monthly', 'None'];
 
 export default function LessonCalControl () {
 
-    const lessonDayRadio = lessonDayArr.map(day=> (
-                    <label className="lesson-control-radio" key={day.toString()} >
-                        {day}
-                        <input type="radio" id="dayRad" />
+    const [date, setDate] = useState('');
+    const [day, setDay] = useState('');
+    const [repeat, setRepeat] = useState('');
+    const [time, setTime] = useState('');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('open');
+    const [link, setLink] = useState('https://www.discord.com');
+
+    const onDateChange = e => setDate(e.target.value);
+    const onDayChange = e => setDay(e.target.value);
+    const onRepeatChange = e => setRepeat(e.target.value);
+    const onTimeChange = e => setTime(e.target.value);
+    const onNameChange = e => setName(e.target.value);
+    const onStatusChange = e => setStatus(e.target.value);
+    const onLinkChange = e => setLink(e.target.value);
+
+    const lessonDayRadio = lessonDayArr.map(dayOfWeek=> (
+                    <label className="lesson-control-radio" key={dayOfWeek.toString()} >
+                        {dayOfWeek}
+                        <input 
+                            type="radio" 
+                            id="dayRad"
+                            name="dayRad"
+                            value={day}
+                            onChange={onDayChange} />
                     </label>
               
             ));
 
-        const repeatOptRadio = repeatOptArr.map(option=> (
-                        <label className="lesson-control-radio" key={option.toString()} >
-                            {option}
-                            <input type="radio" id="optRad" />
-                        </label>
-                  
-                ));
+    const repeatOptRadio = repeatOptArr.map(option=> (
+                    <label className="lesson-control-radio" key={option.toString()} >
+                        {option}
+                        <input 
+                            type="radio" 
+                            id="repeatRad"
+                            name="repeatRad"
+                            value={repeat}
+                            onChange={onRepeatChange} />
+                    </label>
+                
+            ));
     return (
         <div className="lessonControl">
             <form id="lessonControlEle">
-                <p id="lessonControlP">Set New Lesson Date -or- Select New Lesson Day</p>
+                <p class="lessonControlP">
+                    Set New Lesson Date -or- Select Repeat Options
+                </p>
                 <div className='lesson-date-input 
                     container 
                     d-flex-column justify-content-center align-items-center'>
                     <div className="row w-50" >        
-                    <input type="date" name="lesson-date" />
+                    <input 
+                        type="date" 
+                        name="lesson-date"
+                        id="lesson-date"
+                        value={date}
+                        onChange={onDateChange} />
                     </div>
-                    <div>
-                        {lessonDayRadio}
-                    </div>
+                    
                 </div>
             </form>
             <form id="lessonControlEle">
-                <p  id="lessonControlP">Repeat Lesson</p>
-                {repeatOptRadio}
+                <p  class="lessonControlP">Repeat Lesson</p>
+                    {repeatOptRadio}
+                <div>
+                    {lessonDayRadio}
+                </div>
             </form>
+            <form>
+                    <p class="lessonControlP">
+                        Set New Lesson Time
+                    </p>
+                    <div className='lesson-time-input 
+                        container 
+                        d-flex-column justify-content-center align-items-center'>
+                        <div className="row w-50" >        
+                        <input 
+                            type="time" 
+                            name="lesson-time"
+                            id="lesson-time"
+                            value={time}
+                            onChange={onTimeChange} />
+                        </div>
+                    </div>
+                </form>
             <div className="lessonControlBottom">
                 <form className="lessonControlBtn" id="lessonControlEle">
-                    <button className='btn btn-primary'>Create Lessson</button>
+                    <button 
+                        className='btn btn-primary'
+                        id="lessonSubmit"
+                        name="lessonSubmit"
+                        value={link}
+                        onChange={onLinkChange}
+                    >
+                        Create Lessson
+                    </button>
                 </form>
                 <form id="lessonControlEle" > 
                     <label className="studentSelect" id="lessonControlP">
-                        <input type="checkbox" />
+                        <input 
+                            type="checkbox" 
+                            id="lessonStatus"
+                            name="lessonStatus"
+                            value={status}
+                            onChange={onStatusChange}/>
                         Only Available to:
                     </label>
-                        <input type="text" placeholder='Student Name'/>
+                        <input 
+                            type="text" 
+                            placeholder='Student Name'
+                            id="studentName"
+                            name="studentName"
+                            value={name}
+                            onChange={onNameChange}/>
                 </form>
+                
             </div>
         </div>
     )
