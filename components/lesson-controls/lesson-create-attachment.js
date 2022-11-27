46214b2167d94c6fccdf8e1f84b34c5d9f5a0e9d
MoxElliot <eric0952@gmail.com>
2022-11-27 12:54:37 -0800

    attachArray adds <p>
    Using a toggled state for isSelected in the handleAddAttachment, the attachArray now displays each attachment as a <p> after the button is clicked

--- components/lesson-controls/lesson-create-attachment.js
+++ components/lesson-controls/lesson-create-attachment.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import nanoid from 'nanoid';
+
 
 //https://www.pluralsight.com/guides/uploading-files-with-reactjs
 //https://www.pluralsight.com/guides/manipulating-arrays-and-objects-in-state-with-react
@@ -9,44 +9,27 @@ export default function LessonCreateAttachment () {
 
     const [selectedFile, setSelectedFile] = useState();
     const [isSelected, setIsSelected] = useState(false);
-    const [totalAttachments, setTotalAttachments] = useState(0);
     const handleSearchAttachment = (e) => {
         
         setSelectedFile(e.target.files[0].name);
-        setIsSelected(true)
-        attachArray.push(selectedFile)
-       
+        // setIsSelected(false)
+        // attachArray.push(selectedFile)
+        
         return attachArray
     }
 
+    const handleAddAttachment = () => {
+        console.log("includes",attachArray.includes(selectedFile))
+        if(attachArray.includes(selectedFile) || selectedFile === undefined) {
+            setIsSelected(!isSelected)
+        } else {
+        setIsSelected(!isSelected)
+        attachArray.push(selectedFile)
+       }
+        return attachArray
+    }
     console.log("attachArray", attachArray)
-    // const attachArray = [];
-    // const handleAddAttachment = () => {
-    //     console.log("in handleAddAttachment", attachArray)
-    //     attachArray.push(
-    //     selectedFile.name
-    //     )
-    //     return attachArray
-    // }
-
-
-
-    // const addAttachmentText = () => {
-
-    //     console.log(selectedFile.name)
-    //     const attachArray = [];
-    //     for (let i = 0; i < totalAttachments; i++) (
-    //         attachArray.push(
-    //         <div>
-    //             <p>File: {selectedFile.name}</p>
-    //         </div>
-    //         )
-    //     )
-
-    //     return attachArray;
-    // }
     
-
     return ( 
         <form className='lessonControlAttachment m-2 p-2
             col col-md-4 col-6
@@ -54,22 +37,13 @@ export default function LessonCreateAttachment () {
     
             <p className='lessonControlP m-0'>Lesson Attachments</p>
             <input type='file' name='file' onChange={handleSearchAttachment} />
-            {isSelected ? 
-                ( 
-                    <div>
-                        <p>File: {selectedFile}</p>
-                    </div>
-                )
-             : (
-                <p>Select a File...</p>
-            )}
-           {attachArray.map(file => {
-            return(
-            <p>{file}</p>
-            )})}
+           
+                   {attachArray.map(file => 
+                        <p key={file.toString()}>{file}</p>)}
+                 
             <button 
                 className='btn p-0 w-75'
-                // onClick={handleAddAttachment}    
+                onClick={handleAddAttachment}  
             >
                 <label className='bi bi-plus'>Add Attachment</label>
             </button>
